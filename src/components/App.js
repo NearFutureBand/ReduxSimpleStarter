5c6135e5f9407caac65b68ef8939fad72b6fbc68
Pavel.Belyakov <Pavel.Beliakov@itechart-group.com>
2018-11-01 11:30:49 +0300

    making components according to videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar'
+import React, {
+    Component
+} from 'react';
+import SearchBar from './SearchBar';
+import VideoDetail from './VideoDetail';
+import VideoList from './VideoList.jsx';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyCPo4vuh1Y0BxwkPaiNaCkNpGrlD8BpXyk';
 
 export default class App extends Component {
-  render() {
-    return (
-      <SearchBar />
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+        
+        YTSearch({ key: API_KEY, term: 'surfboards'}, (data) => {
+            console.log(data);
+            this.setState({videos: data});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
+    
+    
+    
 }
