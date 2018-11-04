fa08756d865c43cbe639fe022ad631409dde3fa8
Paul White <theflashofsonic@gmail.com>
2018-11-04 23:25:14 +0300

    Form for adding new post layout (not whole)

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/PostsIndex.jsx';
+import PostNew from './components/PostNew.jsx';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-          <Route path="/" component={PostsIndex} />
+         <Switch>
+            <Route path="/posts/new" component={PostNew} />
+            <Route path="/" component={PostsIndex} />
+         </Switch>
+          
       </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
